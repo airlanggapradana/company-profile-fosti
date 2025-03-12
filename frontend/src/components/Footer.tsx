1e58fe287435607882c846101c3e6882b738b8b8
Airlangga Pradana <ranggavfxartwork@gmail.com>
2025-03-12 17:00:34 +0700

    update Footer component to replace Twitter and LinkedIn links with YouTube; adjust layout for better responsiveness

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import React from "react";
 import { Separator } from "./ui/separator";
-import { Github, Instagram, Linkedin, Mail, Twitter } from "lucide-react";
+import { Github, Instagram, Youtube } from "lucide-react";
 import logoputih from "../../public/LOGO FOSTI PUTIH.png";
 import Image from "next/image";
 import { useTheme } from "next-themes";
@@ -13,7 +13,7 @@ const Footer = () => {
     <footer className="border-t bg-background py-12">
       <div className="mx-auto max-w-screen-2xl px-4 py-12 md:px-6">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-4">
-          <div className="md:col-span-2">
+          <div className="md:col-span-1">
             <Link href="#" className="flex items-center space-x-2">
               <Image
                 src={
@@ -38,39 +38,25 @@ const Footer = () => {
           </p>
           <div className="flex items-center space-x-4">
             <Link
-              href="#"
+              href="https://github.com/FOSTI-UMS"
               className="text-muted-foreground transition-colors hover:text-foreground"
             >
               <Github className="h-5 w-5" aria-hidden="true" />
               <span className="sr-only">GitHub</span>
             </Link>
             <Link
-              href="#"
-              className="text-muted-foreground transition-colors hover:text-foreground"
-            >
-              <Twitter className="h-5 w-5" aria-hidden="true" />
-              <span className="sr-only">Twitter</span>
-            </Link>
-            <Link
-              href="#"
+              href="http://instagram.com/fosti_ums"
               className="text-muted-foreground transition-colors hover:text-foreground"
             >
               <Instagram className="h-5 w-5" aria-hidden="true" />
               <span className="sr-only">Instagram</span>
             </Link>
             <Link
-              href="#"
+              href="https://www.youtube.com/@fostiums"
               className="text-muted-foreground transition-colors hover:text-foreground"
             >
-              <Linkedin className="h-5 w-5" aria-hidden="true" />
-              <span className="sr-only">LinkedIn</span>
-            </Link>
-            <Link
-              href="#"
-              className="text-muted-foreground transition-colors hover:text-foreground"
-            >
-              <Mail className="h-5 w-5" aria-hidden="true" />
-              <span className="sr-only">Email</span>
+              <Youtube className="h-5 w-5" aria-hidden="true" />
+              <span className="sr-only">Youtube</span>
             </Link>
           </div>
         </div>
