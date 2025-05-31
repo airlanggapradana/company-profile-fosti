f2b9902913aa7bcbb00a9153760e13192af091fc
Airlangga Pradana <ranggavfxartwork@gmail.com>
2025-05-31 07:45:05 +0700

    feat: reorder executive card display in Divisi component

--- frontend/src/components/Divisi.tsx
+++ frontend/src/components/Divisi.tsx
@@ -60,11 +60,6 @@ const Divisi = () => {
         </p>
       </div>
       <div className="relative mb-16 flex flex-col-reverse items-center gap-8 rounded-2xl bg-gradient-to-tr from-red-500 to-slate-900 p-4 shadow-lg ring-1 ring-red-200/40 md:flex-row md:gap-10 md:p-8">
-        <div className="grid w-full grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-          {bphiTeam.map((executive, index) => (
-            <ExecutiveCard key={index} executive={executive} />
-          ))}
-        </div>
         <div className="mb-4 flex w-full justify-center md:mb-0 md:w-auto">
           <Image
             unoptimized
@@ -73,6 +68,11 @@ const Divisi = () => {
             alt="Fostibar"
             className="h-44 w-80 rounded-xl object-cover shadow-md ring-2 ring-red-300 sm:h-56 sm:w-80 md:h-[18rem] md:w-[28rem] lg:h-[25rem] lg:w-[75rem]"
           />
+        </div>
+        <div className="grid w-full grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+          {bphiTeam.map((executive, index) => (
+            <ExecutiveCard key={index} executive={executive} />
+          ))}
         </div>
       </div>
 
