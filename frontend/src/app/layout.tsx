d88bb7a8393fd411e28c2b002ff530fdf6579a87
Airlangga Pradana <ranggavfxartwork@gmail.com>
2025-08-16 08:36:36 +0700

    feat: add @vercel/speed-insights for performance monitoring

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,6 +5,7 @@ import { type Metadata } from "next";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: {
@@ -26,6 +27,7 @@ export default function RootLayout({
               <Navbar />
               {children}
               <Analytics />
+              <SpeedInsights />
               <Footer />
             </main>
           </AntdRegistry>
